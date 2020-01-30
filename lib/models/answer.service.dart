73bcfa4e5cccc97285bbed745f3ce1d33befb314
William Beh <willbeh@gmail.com>
2020-01-30 10:32:25 +0800

    Change to generated

---
+++ lib/models/answer.service.dart
@@ -0,0 +1,57 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+// **************************************************************************
+// FirebaseServiceGenerator
+// **************************************************************************
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'answer.dart';
+
+class AnswerFirebaseService {
+  static CollectionReference colRef = Firestore.instance.collection('answers');
+
+  static Stream<List<Answer>> find(
+      {Query query, dynamic orderField, bool descending = false}) {
+    Query inColRef = colRef;
+    if (query != null) {
+      inColRef = query;
+    }
+    if (orderField != null) {
+      inColRef = inColRef.orderBy(orderField, descending: descending);
+    }
+    return inColRef.snapshots().map((list) {
+      return list.documents.map((doc) {
+        Map data = doc.data;
+        data['id'] = doc.documentID;
+        return Answer.fromJson(data);
+      }).toList();
+    });
+  }
+
+  static Stream<List<Answer>> findById(
+      {@required String id, dynamic orderField, bool descending = false}) {
+    Query inColRef = colRef;
+    inColRef.where('id', isEqualTo: id);
+    if (orderField != null) {
+      inColRef = inColRef.orderBy(orderField, descending: descending);
+    }
+    return inColRef.snapshots().map((list) {
+      return list.documents.map((doc) {
+        Map data = doc.data;
+        data['id'] = doc.documentID;
+        return Answer.fromJson(data);
+      }).toList();
+    });
+  }
+
+  static Future<DocumentReference> insert(
+      {@required Map<String, dynamic> data}) {
+    return colRef.add(data);
+  }
+
+  static Future<void> update(
+      {@required String id, @required Map<String, dynamic> data}) {
+    return colRef.document(id).updateData(data);
+  }
+}
---
+++ lib/models/series.service.dart
@@ -0,0 +1,57 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+// **************************************************************************
+// FirebaseServiceGenerator
+// **************************************************************************
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'series.dart';
+
+class SeriesFirebaseService {
+  static CollectionReference colRef = Firestore.instance.collection('series');
+
+  static Stream<List<Series>> find(
+      {Query query, dynamic orderField, bool descending = false}) {
+    Query inColRef = colRef;
+    if (query != null) {
+      inColRef = query;
+    }
+    if (orderField != null) {
+      inColRef = inColRef.orderBy(orderField, descending: descending);
+    }
+    return inColRef.snapshots().map((list) {
+      return list.documents.map((doc) {
+        Map data = doc.data;
+        data['id'] = doc.documentID;
+        return Series.fromJson(data);
+      }).toList();
+    });
+  }
+
+  static Stream<List<Series>> findById(
+      {@required String id, dynamic orderField, bool descending = false}) {
+    Query inColRef = colRef;
+    inColRef.where('id', isEqualTo: id);
+    if (orderField != null) {
+      inColRef = inColRef.orderBy(orderField, descending: descending);
+    }
+    return inColRef.snapshots().map((list) {
+      return list.documents.map((doc) {
+        Map data = doc.data;
+        data['id'] = doc.documentID;
+        return Series.fromJson(data);
+      }).toList();
+    });
+  }
+
+  static Future<DocumentReference> insert(
+      {@required Map<String, dynamic> data}) {
+    return colRef.add(data);
+  }
+
+  static Future<void> update(
+      {@required String id, @required Map<String, dynamic> data}) {
+    return colRef.document(id).updateData(data);
+  }
+}
