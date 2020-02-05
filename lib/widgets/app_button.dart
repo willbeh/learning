abf040dd7efed76f8348f8b8e39cbbb92fc29645
William Beh <willbeh@gmail.com>
2020-02-05 16:03:59 +0800

    get max number of series to be shown on home page

--- lib/widgets/app_button.dart
+++ lib/widgets/app_button.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 
 class AppButton {
-  static roundedButton(BuildContext context, {String text, Function onPressed, Widget child, double width = 200, double height = 48, Color textColor, Color color, double borderRadius = 5, double paddingVertical = 15,
+  static roundedButton(BuildContext context, {String text, Function onPressed, Widget child, double width = 200, double height = 30, Color textColor, Color color, double borderRadius = 5, double paddingVertical = 15,
     Color borderColor, double elevation = 1, TextStyle textStyle,
   }) {
     return RaisedButton(
       color: color != null ? color : Theme.of(context).primaryColor,
       child: Container(
+        alignment: Alignment.center,
         padding: EdgeInsets.symmetric(vertical: paddingVertical),
         width: width,
         height: height,
